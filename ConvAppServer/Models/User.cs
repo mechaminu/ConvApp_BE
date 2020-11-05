95b8e3078b52f68efc19cebc2a8723c6effb1280
Minwoo <lmw5201@gmail.com>
2020-11-05 18:59:51 +0900

    declaring blank/template classes for future works

---
+++ ConvAppServer/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConvAppServer.Models
+{
+    public class User
+    {
+        [Key]
+        public string Username { get; set; }
+        public string Userid { get; set; }
+    }
+}
--- ConvAppServer/Program.cs
+++ ConvAppServer/Program.cs
@@ -34,7 +34,5 @@ namespace ConvAppServer
                     .UseIISIntegration()
                     .UseStartup<Startup>();
                 });
-
-
     }
 }
